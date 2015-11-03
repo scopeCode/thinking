cab5001ed294aa6de05f875389ad62d4bef4731b
mo <split@live.cn>
2015-11-03 13:48:27 +0800

    整合 日志的打印引用

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
+//var logger = require('morgan');
+var logger = require("mo/log/index");
+
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -16,11 +17,12 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger.connectLogger);//全局log4j的配置
 
 app.use('/', routes);
 app.use('/users', users);
