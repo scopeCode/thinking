6a7e8f92ca3a55363c0f3d9be604dc6390004db1
mo <split@live.cn>
2015-11-03 14:05:50 +0800

    修正 log4j 的位置

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
-
+var logger = require("mo/log/index").logger("index");
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  logger.info("问为啥笑");
   res.render('index', { title: 'Express' });
 });
 
