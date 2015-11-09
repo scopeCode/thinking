ff7d654f4b3dcfa0e010f9aa7e35532c0b40c3c7
mo <split@live.cn>
2015-11-09 15:13:35 +0800

    调整

--- app.js
+++ app.js
@@ -11,8 +11,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(cookieParser());
 app.use(logger.connectLogger);
 
